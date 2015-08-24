57b52399f991440a9e860c2cc3a05038fdec6de9
Matt Comben <matt.comben@itdev.co.uk>
2015-08-24 21:54:36 +0100

    Fixed backslashes at end of chmod lines, as they are not required

--- source/usr/lib/mcvirt/mcvirt_config.py
+++ source/usr/lib/mcvirt/mcvirt_config.py
@@ -51,8 +51,8 @@ class MCVirtConfig(ConfigFile):
         os.mkdir(MCVirt.ISO_STORAGE_DIR)
 
         # Set permission on MCVirt directory
-        os.chmod(MCVirt.BASE_STORAGE_DIR, stat.S_IRWXU | stat.S_IRGRP \
-                                          | stat.S_IXGRP | stat.S_IROTH \
+        os.chmod(MCVirt.BASE_STORAGE_DIR, stat.S_IRWXU | stat.S_IRGRP 
+                                          | stat.S_IXGRP | stat.S_IROTH 
                                           | stat.S_IXOTH)
         os.chown(MCVirt.BASE_STORAGE_DIR, 0, 0)
 
