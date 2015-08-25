79da30d66db8a999f40c215fb24dd52adf76475a
Matt Comben <matt.comben@itdev.co.uk>
2015-08-25 15:33:46 +0100

    Removed masterdoc from sphinx config

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -53,7 +53,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+#master_doc = 'index'
 
 # General information about the project.
 project = u'mcvirt'
