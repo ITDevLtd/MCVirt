ffc474b6a4c01fd2c9fa1c7f0a5e4e44888f333e
Matt Comben <matt.comben@itdev.co.uk>
2015-08-25 15:02:04 +0100

    Added mock module for libvirt0

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -20,6 +20,16 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('source/usr/lib'))
 
+from unittest.mock import MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['libvirt0']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
