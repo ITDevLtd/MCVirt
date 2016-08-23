de8b89899e10efdd3c6dfec80c6c9cd77095be9f
Matt Comben <matt.comben@itdev.co.uk>
2016-08-23 14:07:41 +0100

    Update unittest after change of --driver to --hdd-driver

--- source/usr/lib/mcvirt/test/virtual_machine/virtual_machine_tests.py
+++ source/usr/lib/mcvirt/test/virtual_machine/virtual_machine_tests.py
@@ -602,7 +602,7 @@ class VirtualMachineTests(TestBase):
                                         ' --network %s --storage-type %s' %
                                         (self.test_vms['TEST_VM_1']['networks'][0],
                                          'Local') +
-                                        ' --driver %s' % disk_driver[0])
+                                        ' --hdd-driver %s' % disk_driver[0])
 
             vm_object = self.vm_factory.getVirtualMachineByName(self.test_vms['TEST_VM_1']['name'])
             self.rpc.annotate_object(vm_object)
