0a8795f94c5ba2181ca36bb6c25c1b5c561fca3d
Matt Comben <matthew@dockstudios.co.uk>
2018-11-04 17:43:55 +0000

    Add caching to certificate generator object, for use in wrap_ssl socket

--- source/mcvirt-daemon/usr/lib/python2.7/dist-packages/mcvirt/rpc/rpc_daemon.py
+++ source/mcvirt-daemon/usr/lib/python2.7/dist-packages/mcvirt/rpc/rpc_daemon.py
@@ -289,6 +289,7 @@ class RpcNSMixinDaemon(object):
     def shutdown(self, signum, frame):
         """Shutdown Pyro Daemon"""
         Syslogger.logger().error('Received signal: %s' % signum)
+        del Pyro4.CERTIFICATE_GENERATOR_FACTORY
         for timer in self.timer_objects:
             Syslogger.logger().info('Shutting down timer: %s' % timer)
             try:
@@ -341,6 +342,9 @@ class RpcNSMixinDaemon(object):
         ]
         for factory_object, name in registration_factories:
             self.register(factory_object, objectId=name, force=True)
+
+        Pyro4.CERTIFICATE_GENERATOR_FACTORY = RpcNSMixinDaemon.DAEMON.registered_factories[
+            'certificate_generator_factory']
 
         Expose.SESSION_OBJECT = RpcNSMixinDaemon.DAEMON.registered_factories['mcvirt_session']
 
