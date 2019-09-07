e19bd937a74569e8410d42a2ef7523a5d9b738f3
Matt Comben <matthew@dockstudios.co.uk>
2019-09-07 08:00:35 +0100

    Update local-dev build script to allow customisation via environment variables

--- scripts/local-dev-buid.sh
+++ scripts/local-dev-buid.sh
@@ -7,9 +7,22 @@ rm *.deb
 ./scripts/build.sh
 ./build.sh
 
-for i in {1..3}
+if [ "x$MCVIRT_VM_PREFIX" == "x" ]
+then
+    MCVIRT_VM_PREFIX=mcvirt
+fi
+if [ "x$MCVIRT_VM_COUNT" == "x" ]
+then
+    MCVIRT_VM_COUNT=3
+fi
+if [ "x$MCVIRT_VM_USER" == "x" ]
+then
+    MCVIRT_VM_USER=root
+fi
+
+for i in $(seq ${MCVIRT_VM_COUNT})
 do
-  ssh root@mcvirt${i} 'rm -f *mcvirt*.deb'
-  scp mcvirt{,-common,-daemon}_*.deb root@mcvirt${i}:~/
-  ssh root@mcvirt${i} 'dpkg -i ./*mcvirt*.deb; apt-get install -f --assume-yes; echo "service mcvirt-ns restart; service mcvirtd restart"'
+  ssh ${MCVIRT_VM_USER}@$MCVIRT_VM_PREFIX${i} 'rm -f *mcvirt*.deb'
+  scp mcvirt{,-common,-daemon}_*.deb ${MCVIRT_VM_USER}@$MCVIRT_VM_PREFIX${i}:~/
+  ssh ${MCVIRT_VM_USER}@${MCVIRT_VM_PREFIX}${i} 'sudo dpkg -i ./*mcvirt*.deb; sudo apt-get install -f --assume-yes; echo "sudo service mcvirt-ns restart; sudo service mcvirtd restart"'
 done
